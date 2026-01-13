88bee8bcad46866b54e8744b32703b1ecbf2ca4a
Akshansh Business <akshansh.d.business@gmail.com>
2026-01-13 15:48:27 +0530

    move

--- utils/contentFulProductPage.js
+++ utils/contentFulProductPage.js
@@ -1,4 +1,4 @@
-const configManager = require('./configManager');
+const configManager = require('./configManager.js');
 
 const environment = process.env.CONTENTFUL_ENVIRONMENT;
 const previewMode = process.env.CONTENTFUL_IS_PREVIEW;
@@ -12,13 +12,13 @@ const client = require('contentful').createClient({
 });
 
 export async function fetchEntries() {
-  const response = await client.getContentType('productDetail');
-  const entries = await client.getEntries();
-
-  console.log(response);
-  console.log(entries);
-
-  if (entries.items) return entries.items;
+  //   const response = await client.getContentType('productDetail');
+  //   const entries = await client.getEntries();
+  //   const types = await client.getContentTypes();
+  //   console.log(types.items.map((t) => t.sys.id));
+  //   console.log(response);
+  //   console.log(entries);
+  //if (entries.items) return entries.items;
 }
 
 export default { fetchEntries };
